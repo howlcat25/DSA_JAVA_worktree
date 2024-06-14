a229b8e22e3d4e234efa9bf6fd3a148e1a69f903
Aditi Dhepe <aditidhepe25@gmail.com>
2024-06-14 23:36:57 +0530

    functions completed

---
+++ Functions.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class Functions {
+
+    public static void Hello(){
+   
+        System.out.println("Hello");
+
+    }
+
+
+
+    public static void main(String[] args) {
+        Hello();
+    }
+}
